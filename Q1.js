9ba4772539ffcf86ed48870e53650691dcdde1e0
trentyap <trent8811@gmail.com>
2023-04-26 21:14:22 +0800

    Commit for Q1

--- Q1.js
+++ Q1.js
@@ -1,2 +1,7 @@
-console.log(123);
-console.log(456);
+function q1(num) {
+
+    for (let i=1; i<=num; i++) {
+
+        console.log(i % 3 === 0 ? (i % 5 === 0 ? 'fizzbuzz' : 'buzz') : 'fizz')
+    }
+}
